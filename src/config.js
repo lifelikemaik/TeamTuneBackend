c282e8b99c238777b3135e35f92d0afbbf1f93e1
David Satomi <d.satomi@outlook.de>
2021-05-30 00:43:12 +0200

    Small fixes regarding models

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 // Configuration variables
 const port = process.env.PORT || "4000";
-const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/moviedb";
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/teamtunedb";
 const JwtSecret = process.env.JWT_SECRET || "very secret secret";
 
 module.exports = {
