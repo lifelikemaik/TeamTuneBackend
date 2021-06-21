7e6072affd5a8b3f21b2710d52accea7780e88ad
Jonas Merforth <jonas.merforth@gmail.com>
2021-06-21 19:24:18 +0200

    Add check for user and comments

--- src/spotifyControllers.js
+++ src/spotifyControllers.js
@@ -16,6 +16,10 @@ function authenticateAPI(user) {
 
 module.exports = {
     getPlaylistSpotify: async function (user, playlistId) {
+        if (!user || !user.access_token || !user.refresh_token) {
+            console.log('Incorrect user object passed.')
+            return null;
+        }
         const spotifyApi = authenticateAPI(user);
 
         // Get a playlist
@@ -30,7 +34,16 @@ module.exports = {
                 });
         })
     },
+    /**
+     * returns all Playlists of a user
+     * @param user the user object which needs to have an access_token and refresh_token
+     * @returns {Promise<unknown>} Promise containing a list of all playlists
+     */
     getUserPlaylistsSpotify: async function (user) {
+        if (!user || !user.access_token || !user.refresh_token) {
+            console.log('Incorrect user object passed.')
+            return null;
+        }
         const spotifyApi = authenticateAPI(user);
 
         return new Promise(function (resolve, reject) {
@@ -48,8 +61,15 @@ module.exports = {
     },
 }
 
+/**
+ * concats playlists through pagination of Spotify API
+ * @param spotifyApi
+ * @param offset current offset (start 0)
+ * @param allPlaylists all Playlists (start [])
+ * @param firstResponse response of the first request
+ * @returns {Promise<*>} Promise containing array of all playlists
+ */
 async function getAllUserPlaylists(spotifyApi, offset, allPlaylists, firstResponse) {
-    //console.log('firstResponse: ', firstResponse);
     console.log('firstResponse.next: ', !!firstResponse.next);
     while (!!firstResponse?.next) {
         console.log('looping');
@@ -65,7 +85,7 @@ async function getAllUserPlaylists(spotifyApi, offset, allPlaylists, firstRespon
 }
 
 async function getAllTracks(requestFunction, offset, allTracks, firstResponse) {
-    while (firstResponse.tracks.next) {
+    while (!!firstResponse?.tracks?.next) {
 
     }
 }
